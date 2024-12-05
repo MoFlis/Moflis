ae58a1a84e373e7bc51642df41061dfcca5fb7d2
JuHyun <aajj9776@hanmail.net>
2024-12-06 01:08:32 +0900

    초기설정

---
+++ src/test/java/com/project/moflis/MoflisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moflis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoflisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
