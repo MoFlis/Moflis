8a63c1b156fd4abacfcc40f5d24dc9af453a095e
JuHyun <aajj9776@hanmail.net>
2025-01-03 04:46:50 +0900

    [MOFLIS] #6 프로필 기능 스네이크케이스 수정

--- src/main/java/com/project/moflis/util/FileNameConflictResolver.java
+++ src/main/java/com/project/moflis/util/FileNameConflictResolver.java
@@ -7,7 +7,7 @@ public class FileNameConflictResolver {
     public static String checkSameFileName(String fileName, String path) {
         int period = fileName.lastIndexOf(".");// test23.txt --> 6
 
-        String f_name = fileName.substring(0, period);// test23
+        String file = fileName.substring(0, period);// test23
         String suffix = fileName.substring(period); // .txt
 
         String saveFilePath = path + "/" + fileName;
@@ -18,7 +18,7 @@ public class FileNameConflictResolver {
 
         while (f != null && f.exists()) {
             StringBuffer sb = new StringBuffer();
-            sb.append(f_name);
+            sb.append(file);
             sb.append(idx++);
             sb.append(suffix);
 
