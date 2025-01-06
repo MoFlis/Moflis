c9b3d42180ac41a9510542b7f2afcd09721f4903
Juhyun <aajj9776@hanmail.net>
2025-01-06 15:38:53 +0900

    [MOFLIS] #6프로필 기능 gitignore, url 수정

--- src/main/java/com/project/moflis/controller/ProfileController.java
+++ src/main/java/com/project/moflis/controller/ProfileController.java
@@ -2,38 +2,33 @@ package com.project.moflis.controller;
 
 import com.project.moflis.dto.ProfilesDTO;
 import com.project.moflis.service.ProfileService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/profiles")
-public class ProfilesController {
+@RequestMapping("/api/v1/users/{userId}")
+public class ProfileController {
 
     private final ProfileService profileService;
 
-    public ProfilesController(ProfileService profileService) {
+    public ProfileController(ProfileService profileService) {
         this.profileService = profileService;
     }
 
 
-    @GetMapping("/{userId}")
+    @GetMapping("/profile")
     public ProfilesDTO getProfile(@PathVariable int userId) {
         ProfilesDTO profile = profileService.getProfiles(userId);
         return profile;
     }
 
-    @PostMapping("/{userId}")
+    @PostMapping("/profile")
     public ProfilesDTO addProfile(@PathVariable int userId, ProfilesDTO profilesDTO) {
         profilesDTO.setUserId(userId);
         ProfilesDTO addResult = profileService.addProfile(profilesDTO);
         return addResult;
     }
 
-    @PatchMapping("/{userId}")
+    @PatchMapping("/profile")
     public ProfilesDTO updateProfile(@PathVariable int userId, ProfilesDTO profileInfo) {
         profileInfo.setUserId(userId);
         ProfilesDTO profile = profileService.updateProfiles(profileInfo);
