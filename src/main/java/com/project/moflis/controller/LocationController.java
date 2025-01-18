41656cabb97135da17be06454d9e396bc41fbb86
Juhyun <aajj9776@hanmail.net>
2025-01-19 03:45:03 +0900

    [MOFILS] #8위치 인증 메서드 이름 변경

--- src/main/java/com/project/moflis/controller/LocationController.java
+++ src/main/java/com/project/moflis/controller/LocationController.java
@@ -23,7 +23,7 @@ public class LocationController {
     }
 
     @PostMapping("/location-verify")
-    public ResponseEntity<Boolean> locationVerify(@PathVariable("userId") Integer userId, @RequestBody VerifyLocationRequest request) {
+    public ResponseEntity<Boolean> checkLocation(@PathVariable("userId") Integer userId, @RequestBody VerifyLocationRequest request) {
         boolean isVerify = locationApplicationService.verifyLocation(userId, request);
         return ResponseEntity.ok(isVerify);
     }
