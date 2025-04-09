878e84fd17489a6c9cb5b8511c8495d574196648
Juhyun <aajj9776@hanmail.net>
2025-04-09 14:42:56 +0900

    [MOFLIS] #16 매칭글 기능 participants로 수정

--- src/main/java/com/project/moflis/participant/controller/ParticipantController.java
+++ src/main/java/com/project/moflis/participant/controller/ParticipantController.java
@@ -7,19 +7,13 @@ import com.project.moflis.participant.enums.ParticipantStatus;
 import com.project.moflis.participant.service.ParticipantService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users/participant")
+@RequestMapping("/api/v1/participants")
 public class ParticipantController {
 
     private final ParticipantService participantService;
