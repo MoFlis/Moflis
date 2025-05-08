4abc49ef411c43fc40ddf23ac2d20aeca087fc19
Juhyun <aajj9776@hanmail.net>
2025-05-08 12:05:38 +0900

    [MOFLIS] #18 유저 기능 setter 삭제

--- src/main/java/com/project/moflis/user/dto/request/JoinUserRequest.java
+++ src/main/java/com/project/moflis/user/dto/request/JoinUserRequest.java
@@ -3,13 +3,11 @@ package com.project.moflis.user.dto.request;
 import com.project.moflis.user.command.JoinUserCommand;
 import com.project.moflis.user.enums.UserStatus;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 public class JoinUserRequest {
 
     private Long id;
