0d92b5c6a441eb1499975a48481f3c78b18f0c94
Juhyun <aajj9776@hanmail.net>
2025-05-18 01:29:38 +0900

    [MOFLIS] #21 리뷰기능 조회, 수정

--- src/main/java/com/project/moflis/review/service/ReviewService.java
+++ src/main/java/com/project/moflis/review/service/ReviewService.java
@@ -4,13 +4,17 @@ import com.project.moflis.participant.service.ParticipantService;
 import com.project.moflis.post.entity.Post;
 import com.project.moflis.post.service.PostService;
 import com.project.moflis.review.dto.request.ReviewRequest;
+import com.project.moflis.review.dto.request.ReviewUpdateRequest;
 import com.project.moflis.review.dto.response.ReviewResponse;
 import com.project.moflis.review.entity.Review;
 import com.project.moflis.review.mapper.ReviewMapper;
 import com.project.moflis.review.repository.ReviewRepository;
 import com.project.moflis.user.entity.User;
 import com.project.moflis.user.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewService {
@@ -27,6 +31,12 @@ public class ReviewService {
         this.userService = userService;
         this.postService = postService;
         this.reviewMapper = reviewMapper;
+    }
+
+
+    public Page<ReviewResponse> getReviewsByRevieweeId(Long revieweeId, Pageable pageable) {
+        return reviewRepository.findByRevieweeId(revieweeId, pageable)
+                .map(reviewMapper::toReviewResponse);
     }
 
     public ReviewResponse addReview(Long reviewerId, ReviewRequest request) {
@@ -49,5 +59,20 @@ public class ReviewService {
 
         Review review = Review.toEntity(reviewer, reviewee, post, request.rating(), request.content());
         return reviewMapper.toReviewResponse(reviewRepository.save(review));
+    }
+
+
+    @Transactional
+    public ReviewResponse updateReview(Long currentUserId, Long reviewId, ReviewUpdateRequest request) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new RuntimeException("리뷰를 찾을 수 없습니다."));
+
+        if (!review.getReviewer().getId().equals(currentUserId)) {
+            throw new RuntimeException("리뷰를 수정할 권한이 없습니다.");
+        }
+
+        review.update(request.rating(), request.content());
+
+        return reviewMapper.toReviewResponse(review);
     }
 }
