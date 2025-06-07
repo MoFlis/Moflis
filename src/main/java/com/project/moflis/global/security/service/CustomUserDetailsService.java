65d9f0d88f15dc31f953aa25e625cdbfd2a3fcac
Juhyun <aajj9776@hanmail.net>
2025-06-07 15:54:28 +0900

    [MOFLIS] userRepository 내에서 처리

--- src/main/java/com/project/moflis/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/project/moflis/global/security/service/CustomUserDetailsService.java
@@ -19,9 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
-
+        User user = userRepository.findByEmailOrThrow(email);
         return new CustomUserDetails(user);
     }
 }
