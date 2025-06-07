2747ee2300d2c178171e44faa86f257218a2261f
Juhyun <aajj9776@hanmail.net>
2025-06-07 15:38:04 +0900

    [MOFLIS] loginusercommand final 선언

--- src/main/java/com/project/moflis/user/command/LoginUserCommand.java
+++ src/main/java/com/project/moflis/user/command/LoginUserCommand.java
@@ -1,26 +1,40 @@
 package com.project.moflis.user.command;
 
 import com.project.moflis.user.enums.UserStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @Getter
 public class LoginUserCommand {
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
-    private String phone;
-    private LocalDate birth;
-    private String address;
-    private String nickname;
-    private boolean gender;
-    private int kakao;
-    private LocalDateTime joinDate;
-    private UserStatus userStatus;
-    private String grade;
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
+    private final String phone;
+    private final LocalDate birth;
+    private final String address;
+    private final String nickname;
+    private final boolean gender;
+    private final int kakao;
+    private final LocalDateTime joinDate;
+    private final UserStatus userStatus;
+    private final String grade;
+
+    public LoginUserCommand(Long id, String name, String email, String password, String phone, LocalDate birth, String address, String nickname, boolean gender, int kakao, LocalDateTime joinDate, UserStatus userStatus, String grade) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.birth = birth;
+        this.address = address;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.kakao = kakao;
+        this.joinDate = joinDate;
+        this.userStatus = userStatus;
+        this.grade = grade;
+    }
 }
