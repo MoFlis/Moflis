5754a42d856703ebb432b1b4a6d9a0211724eddf
Juhyun <aajj9776@hanmail.net>
2025-06-07 15:35:17 +0900

    [MOFLIS] loginuserresponse final 선언

--- src/main/java/com/project/moflis/user/dto/response/LoginUserResponse.java
+++ src/main/java/com/project/moflis/user/dto/response/LoginUserResponse.java
@@ -1,16 +1,21 @@
 package com.project.moflis.user.dto.response;
 
 import com.project.moflis.user.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class LoginUserResponse {
-    private Long id;
-    private String name;
-    private String email;
-    private String accessToken;
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String accessToken;
+
+    public LoginUserResponse(Long id, String name, String email, String accessToken) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.accessToken = accessToken;
+    }
 
     public static LoginUserResponse from(User user, String accessToken) {
         return new LoginUserResponse(
