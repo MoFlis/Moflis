f6edec8f5111f3c80a27a046662e8f54aa406830
Juhyun <aajj9776@hanmail.net>
2025-06-28 13:45:36 +0900

    [MOFLIS] #24 이메일 인증 트랜잭션 안에서 외부api 호출안하게 수정

--- src/main/java/com/project/moflis/emailverification/scheduler/VerificationTokenRetryScheduler.java
+++ src/main/java/com/project/moflis/emailverification/scheduler/VerificationTokenRetryScheduler.java
@@ -22,36 +22,44 @@ public class VerificationTokenRetryScheduler {
     }
 
     @Scheduled(fixedDelay = 5 * 60 * 1000)
-    @Transactional
     public void retryFailedEmails() {
         List<VerificationToken> failedTokens = tokenRepository.findByStatus(TokenStatus.FAILED);
 
         for (VerificationToken token : failedTokens) {
-            try {
-                emailService.sendEmail(
-                        token.getEmail(),
-                        "회원가입 이메일 인증 안내",
-                        """
-                                안녕하세요.
-                                
-                                MOFLIS 서비스 회원가입을 진행해주셔서 감사합니다.
-                                
-                                인증번호: [%s]
-                                
-                                만약 회원가입을 요청하지 않으셨다면 본 이메일은 무시하셔도 됩니다.
-                                
-                                감사합니다.
-                                - MOFLIS 서비스팀 -
-                                """.formatted(token)
-                );
+            boolean success = sendVerificationEmail(token);
 
-                token.setStatus(TokenStatus.SENT);
-            } catch (Exception e) {
-                token.setStatus(TokenStatus.FAILED);
-            }
-            tokenRepository.save(token);
+            updateTokenStatus(token, success);
         }
+    }
 
+    private boolean sendVerificationEmail(VerificationToken token) {
+        try {
+            emailService.sendEmail(
+                    token.getEmail(),
+                    "회원가입 이메일 인증 안내",
+                    """
+                            안녕하세요.
+                            
+                            MOFLIS 서비스 회원가입을 진행해주셔서 감사합니다.
+                            
+                            인증번호: [%s]
+                            
+                            만약 회원가입을 요청하지 않으셨다면 본 이메일은 무시하셔도 됩니다.
+                            
+                            감사합니다.
+                            - MOFLIS 서비스팀 -
+                            """.formatted(token)
+            );
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Transactional
+    public void updateTokenStatus(VerificationToken token, boolean success) {
+        token.setStatus(success ? TokenStatus.SENT : TokenStatus.FAILED);
+        tokenRepository.save(token);
     }
 
 }
